6e986c41f532ffe75e5d215da49cf1eb8de8f7ce
Bob Aman <bob@sporkmonger.com>
2015-03-20 21:32:25 +0300

    Report code coverage to Code Climate.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 group :test do
   gem 'coveralls', require: false
+  gem 'codeclimate-test-reporter', require: false
 end
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rspec'
 require 'coveralls'
 Coveralls.wear!
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
 require 'duet/version'
 
