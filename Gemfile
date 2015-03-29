75ad6bb3525e9365ad55a498129d1d7e613dc13f
Bob Aman <bob@sporkmonger.com>
2015-03-29 16:13:35 +0300

    Run Coveralls and Code Climate in CI environment only.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 group :test do
+  gem 'simplecov', require: false
   gem 'coveralls', require: false
   gem 'codeclimate-test-reporter', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,18 @@
 require 'rspec'
-require 'coveralls'
-Coveralls.wear!
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+
+if ENV['CI'].to_s == 'true'
+  require 'coveralls'
+  Coveralls.wear! do
+    add_filter 'spec/'
+  end
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'spec/'
+  end
+end
 
 require 'duet/version'
 
